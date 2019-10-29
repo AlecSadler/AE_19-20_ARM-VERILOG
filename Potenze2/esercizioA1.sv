32ad881a15afa0d92b0641dd784f385133a0943c
Salvo Firera <45435648+AlecSadler@users.noreply.github.com>
2019-10-29 18:00:11 +0100

    Update esercizioA1.sv

--- Potenze2/esercizioA1.sv
+++ Potenze2/esercizioA1.sv
@@ -1,4 +1,4 @@
-module msb1 (output z,input [0:7]x);
+module pow2 (output z,input [0:7]x);
 
   assign z= (x[0] & !x[1] & !x[2] & !x[3] & !x[4] & !x[5] & !x[6] & !x[7]) |
             (!x[0] & x[1] & !x[2] & !x[3] & !x[4] & !x[5] & !x[6] & !x[7]) |
