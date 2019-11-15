087c1f761cf4c26d2c9e1996faaeeb9ecb9bdb54
Salvo Firera <45435648+AlecSadler@users.noreply.github.com>
2019-11-15 22:11:00 +0100

    Update str_occorrenze.s

--- Assembler/str_occorrenze.s
+++ Assembler/str_occorrenze.s
@@ -4,25 +4,25 @@ ch:  .ascii "a"
 .text
 .global main
 
-main:	mov r1,#0
+main:	    mov r1,#0
 	    bl check
 	    mov r1,r0
 	    mov r7,#1
-    	svc 0
+    	    svc 0
 
-check:	ldr r2,=str
-	      ldr r3,=ch
-	      mov r0,#0
-      	ldrb r3,[r3]
-        push {r4,r5}
-      	mov r4,#0
-      	loop:	ldrb r5,[r2,r4]
+check:	    ldr r2,=str
+	    ldr r3,=ch
+	    mov r0,#0
+      	    ldrb r3,[r3]
+            push {r4,r5}
+      	    mov r4,#0
+      	    loop:	ldrb r5,[r2,r4]
 	          	cmp r5, #0
-		          beq out
+		        beq out
 	          	cmp r5,r3
 	          	addeq r0,r0,#1
 	          	add r4,r4,#1
           		b loop
-	      out:
-        pop {r4,r5}
-	      mov pc,lr
+	    out:
+            pop {r4,r5}
+	    mov pc,lr
