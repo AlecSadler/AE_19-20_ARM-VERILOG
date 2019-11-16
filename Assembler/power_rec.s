21c58bb145286f5c2a5899e2ffd2057b16b01a04
Salvo Firera <45435648+AlecSadler@users.noreply.github.com>
2019-11-16 22:04:52 +0100

    Add files via upload

---
+++ Assembler/power_rec.s
@@ -0,0 +1,20 @@
+.text
+.global main
+
+main:	mov r0,#4               @base
+	    mov r1,#3               @esponente
+	    bl power
+	    mov r7,#1
+	    svc 0
+
+power:
+	    cmp r1,#0               @caso base
+	    bgt else
+	    mov r0,#1
+	    mov pc,lr
+	    else:	push {r0,lr}
+		        sub r1,r1,#1
+		        bl power
+		        pop {r2,lr}      @ripristino r0 su r2
+		        mul r0,r2,r0
+		        mov pc,lr
